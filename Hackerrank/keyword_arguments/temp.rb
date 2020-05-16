98a2c1ebf6ea86f097f91289dd3507d5a9d58b1f
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-16 22:50:53 +0200

    conversion formulas

--- Hackerrank/keyword_arguments/temp.rb
+++ Hackerrank/keyword_arguments/temp.rb
@@ -3,33 +3,27 @@ def convert_temp(temp, input_scale: '', **options)
   case input_scale.downcase
   when 'celsius'
     if option == 'fahrenheit'
-      ctof = (temp * (5.0 / 9.0) + 32).truncate(2)
+      res = (temp * (5.0 / 9.0) + 32).truncate(2)
     elsif option == 'kelvin'
-      ctok = temp + 273.15
+      res = (temp + 273.15)
     end
   when 'fahrenheit'
     if option == 'celsius'
-      ftoc = ((temp.to_f - 32) * (5.0 / 9.0)).truncate(2)
+      res = ((temp - 32) * (5.0 / 9.0)).truncate(2)
     elsif option == 'kelvin'
-      ftok = ((temp.to_f - 32) * (5.0 / 9) + 273.15).truncate(2)
+      res = ((temp - 32) * (5.0 / 9.0) + 273.15).truncate(2)
     end
   when 'kelvin'
     if option == 'celsius'
-      ktoc = temp - 273.15
+      res = (temp - 273.15).truncate(2)
     elsif option == 'fahrenheit'
-      ktof = ((temp - 273.15) * (9.0 / 5) + 32).round(2)
+      res = (temp * (9.0 / 5) + 32 + 273.15).truncate(2)
     end
   else
     puts "sugi o ceapa.."
   end
+    res
 end
-
-# (0°C × 9/5) + 32 = 32°F
-# 0°C + 273.15 = 273,15K
-# (0°F − 32) × 5/9 = -17,78°C
-# (0°F − 32) × 5/9 + 273.15 = 255,372K
-# 0K − 273.15 = -273,1°C
-# (0K − 273.15) × 9/5 + 32 = -459,7°F
 
 p convert_temp(0, input_scale: 'celsius', output_scale: 'kelvin')
 p convert_temp(0, input_scale: 'celsius', output_scale: 'fahrenheit')
